8082ba434ea9292851fa26d46827ec5da9c9d862
Mihail Schupakevich <m.schupakevich@nv.dunice.net>
2024-04-08 16:44:45 +0300

    Index.js...

--- index.js
+++ index.js
@@ -15,7 +15,6 @@ let curPage = 1;
 const VIEW_COUNT = 5;
 const DBCLICK_NODE = 2;
 const { _ } = window;
-//
 const mainInput = document.getElementById('taskInput');
 const btnTask = document.getElementById('addButton');
 const ulTask = document.getElementById('ul');
@@ -23,15 +22,12 @@ const globalCheckbox = document.getElementById('globalCheckbox');
 const allDeleteComplete = document.getElementById('allDeleteComplete');
 const allButtons = document.getElementById('allButtons');
 const pgContainer = document.getElementById('pageContainer');
-
 let arrayTask = [];
 
 const goToNextPage = () => {
   const countPage = Math.ceil(arrayTask.length / VIEW_COUNT);
   if ((countPage) > (curPage)) {
-    curPage = countPage;
-  }
-};
+    curPage = countPage;}};
 const typeBtnSwitch = () => {
   switch (filterType) {
     case 'activeTasks':
@@ -39,17 +35,14 @@ const typeBtnSwitch = () => {
     case 'completedTasks':
       return arrayTask.filter((obj) => obj.isChecked);
     default:
-      return arrayTask;
-  }
-};
+      return arrayTask;}};
 
 const goToPrevPage = () => {
   const filteredTasks = typeBtnSwitch();
   const numberOfPage = Math.ceil(filteredTasks.length / VIEW_COUNT);
   curPage = numberOfPage < curPage
     ? numberOfPage
-    : curPage;
-};
+    : curPage;};
 
 const createPageButtons = (tasksLength) => {
   const pagesCount = Math.ceil(tasksLength / VIEW_COUNT);
@@ -57,11 +50,9 @@ const createPageButtons = (tasksLength) => {
   for (let i = 1; i <= pagesCount; i += 1) {
     const active = i === curPage ? DANGER_STYLE : PRIMARY_STYLE;
     htmlList += `
-          <button type="button" data-bs-toggle="button" class="${active}">${i}</button>
-          `;
-  }
-  pgContainer.innerHTML = htmlList;
-};
+          <button type="button" data-bs-toggle="button" class="${active}">${i}</button>`;}
+  pgContainer.innerHTML = htmlList;};
+
 const deleteSpace = () => mainInput.value.replace(/\s+/g, ' ').trim();
 
 function rendering() {
@@ -76,15 +67,12 @@ function rendering() {
           <span type="submit" class="customclass overflow-auto ms-2 me-2">${element.text}</span>
           <input hidden  id="section-input" class="ms-5 me-5  overflow-auto form-control form-control-md" value="${element.text}"></input>
           <button   type="submit" class="btn-close float-sm-right ml-auto float-right" aria-label="Close"></button>
-          </li>
-          `;
-  });
+          </li>`;});
   ulTask.innerHTML = htmlList;
   styleButtonRender();
   createPageButtons(filteredTasks.length);
   globalCheckbox.checked = arrayTask.length !==0 ? arrayTask.every((item) => item.isChecked)
-    :false;
-}
+    :false;}
 
 const addTask = () => {
   const onlyText = deleteSpace();
@@ -92,15 +80,12 @@ const addTask = () => {
     const Task = {
       text: _.escape(onlyText),
       id: Date.now(),
-      isChecked: false,
-    };
+      isChecked: false,};
     filterType = ALL;
     arrayTask.push(Task);
     goToNextPage();
     rendering();
-    mainInput.value = '';
-  }
-};
+    mainInput.value = '';}};
 
 const  buttonRefresh=() =>
 {   allButtons.children[0].setAttribute('class', PRIMARY_STYLE);
@@ -113,45 +98,35 @@ const  buttonRefresh=() =>
       allButtons.children[1].setAttribute('class', DANGER_STYLE);
     }
     if (filterType === 'completedTasks') {
-      allButtons.children[2].setAttribute('class', DANGER_STYLE);
-    }
-};
+      allButtons.children[2].setAttribute('class', DANGER_STYLE);}};
+
 const styleButtonRender = () => {
   buttonRefresh();
   allButtons.children[0].textContent = `Все задачи`;
   allButtons.children[2].textContent = `Выполненные задачи`;
-  allButtons.children[1].textContent = `Активные задачи`;
-};
+  allButtons.children[1].textContent = `Активные задачи`;};
 
 const Enter = (e) => {
   const key = e.which || e.keyCode;
   if (key === 13) {
-    btnTask.click();
-  }
-};
+    btnTask.click();}};
+
 const editTask = (e) => {
   const taskId = e.target.parentNode.getAttribute(DATA_ID);
   const SravnIndex = arrayTask.findIndex((obj) => obj.id === Number(taskId));
   if (e.target.tagName === BUTTON) {
     arrayTask.splice(SravnIndex, 1);
     goToPrevPage();
-    rendering();
-  }
-
+    rendering();}
   if (e.target.type === CHECKBOX) {
     arrayTask[SravnIndex].isChecked = e.target.checked;   
     goToPrevPage();
-    rendering();
-    
-  }
-
+    rendering();}
   if ((e.target.tagName === SPAN) && (e.detail === DBCLICK_NODE)) {
     e.target.removeAttribute(READ_ONLY);
     e.target.style.display = NONE;
     e.target.parentNode.children[2].hidden = false;
-    e.target.parentNode.children[2].focus()
-  }
-};
+    e.target.parentNode.children[2].focus()}};
 
 const saveChanges = (event) => {
   event.target.parentNode.children[2].value = _.escape(event.target.parentNode.children[2].value.replace(/\s+/g, ' ').trim());
@@ -159,50 +134,38 @@ const saveChanges = (event) => {
     const taskId = event.target.parentNode.getAttribute(DATA_ID);
     const currentTask = arrayTask.find((obj) => obj.id === Number(taskId));
     currentTask.text = event.target.parentNode.children[2].value;
-    rendering();
-  } else {
-    rendering();
-  }
-};
+    rendering();} 
+    else {
+    rendering();}};
 
 const saveOrExit = (event) => {
   if (event.keyCode === 27) {
     rendering();
   }
   if (event.keyCode === 13) {
-    saveChanges(event);
-  }
-};
-const fullCheckbox = () => {
-  
-  arrayTask.forEach((element) => {
-    
-    element.isChecked = globalCheckbox.checked
-    
+    saveChanges(event);}};
 
-  }); 
-  rendering(); 
-};
+const fullCheckbox = () => {
+  arrayTask.forEach((element) => {
+    element.isChecked = globalCheckbox.checked}); 
+  rendering(); };
+
 const dltCompleteTasks = () => {
   arrayTask = arrayTask.filter((element) => element.isChecked === false);
-  rendering();
-};
+  rendering();};
+
 const changeFilterType = (event) => {
   if (event.target.tagName === BUTTON) {
     event.target.setAttribute(CLASS, DANGER_STYLE);
     const buttonId = event.target.getAttribute(ID);
     filterType = buttonId;
     curPage = 1;
-    rendering();
-  }
-};
+    rendering();}};
 
 const changePage = (event) => {
   if (event.target.tagName === BUTTON) {
     curPage = Number(event.target.textContent);
-    rendering();
-  }
-};
+    rendering();}};
 
 btnTask.addEventListener('click', addTask);
 globalCheckbox.addEventListener('click', fullCheckbox);
